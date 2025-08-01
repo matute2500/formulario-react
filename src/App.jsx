7b0dcb55b4f25c79c91896aac996e06c93069284
matute2500 <36451218+matute2500@users.noreply.github.com>
2025-08-01 08:57:52 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -56,16 +56,7 @@ function Formulario() {
           className="input"
           required
         />
-        <label style={styles.label}>
-          Fecha:
-          <input
-            type="date"
-            value={fecha}
-            onChange={(e) => setFecha(e.target.value)}
-            required
-            style={styles.input}
-          />
-        </label>
+        
 
         <label>Tipo:</label>
         <select
